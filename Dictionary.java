88a9f9edc32325bd284af731890c2bb330601e57
Jeff <ma.jeff126@gmail.com>
2022-02-13 15:29:56 -0800

    Worked out Distinct, Reverse, Parts Of Spreech methods

--- Dictionary.java
+++ Dictionary.java
@@ -8,6 +8,7 @@ public class Dictionary {
         boolean running = true;
         Scanner in = new Scanner(System.in);
         HashMap<String, ArrayList<dictData>> hmap = new HashMap<String, ArrayList<dictData>>();
+        String[] partOfSpeeches = {"noun", "verb", "adjective", "adverb", "pronoun", "preposition", "conjunction", "interjection"};
 
         System.out.println("! Loading data..."); 
 
@@ -65,19 +66,71 @@ public class Dictionary {
                 System.out.println("-----THANK YOU-----");
                 break;
             }
-            else if(hmap.containsKey(word)) {
+            else if(hmap.containsKey(word)) { //Checks if hmap has the word
                 ArrayList<dictData> pull = hmap.get(word);
 
-                for(dictData data : pull) {
+                for(dictData data : pull) { //Loops through the ArrayList<dictdata> for the definitions and prints them
                     System.out.println("   " + data);
                 }
-                //System.out.println(hmap.get(word)); //Drops every def without spacing needs fix
             }
-            else if(!hmap.containsKey(word)) {
+            else if(!hmap.containsKey(word)) { // does this if hmap doesnt have it
                 System.out.println("   <NOT FOUND> To be considered for the next release. Thank you.");
             }
 
             System.out.println("   |");
         }while(running);
+    }
+
+    public static ArrayList<dictData> returnSamePartsOfSpeechAndReverse(ArrayList<dictData> data, String partOfSpeech, boolean reverse) { //Should return the list based on parts of speech and reversed 
+        ArrayList<dictData> sortedData = new ArrayList<dictData>();
+        if(data != null) {
+            if(reverse = true) { //Reverses the returned list
+                for(dictData list : data) {
+                    ArrayList<dictData> regList = new ArrayList<dictData>();
+                    if(list.getPartOfSpeech().equalsIgnoreCase(partOfSpeech)) { //Assigns values to the Arraylist
+                        regList.add(list);
+                    }
+                    for(int i = regList.size() -1 ; i >= 0 ; i--) { //Loops through intiial list and assigns the reversed values to the returned ArrayList
+                        sortedData.add(regList.get(i));
+                    }
+                }
+            }
+            else {
+                for(dictData list : data) {
+                    if(list.getPartOfSpeech().equalsIgnoreCase(partOfSpeech)) { 
+                        sortedData.add(list);
+                    }
+                }
+            }
+        }
+        return sortedData;
+    }
+    
+    public static ArrayList<dictData> returnSamePartsOfSpeech(ArrayList<dictData> data, String partOfSpeech) { // Returns just nouns or w,e
+        ArrayList<dictData> sortedData = new ArrayList<dictData>();
+        for(dictData list : data) {
+            if(list.getPartOfSpeech().equalsIgnoreCase(partOfSpeech)) { 
+                sortedData.add(list);
+            }
+        }
+        return sortedData;
+    }
+
+    public static ArrayList<dictData> distinctRemoval(ArrayList<dictData> data) { //Distinct - removes duplicates from output
+        ArrayList<dictData> sortedData = new ArrayList<dictData>();
+        for(int i = 0; i < data.size(); i++) { 
+            if(sortedData.contains(data.get(i))) {
+                sortedData.add(data.get(i));
+            }
+        }
+        return sortedData;
+    }
+
+    public static ArrayList<dictData> reverseList(ArrayList<dictData> data) { //Reverses the list and returns reversed list
+        ArrayList<dictData> sortedData = new ArrayList<dictData>();
+        for(int i = data.size() -1 ; i >= 0 ; i--) { //Loops through ArrayList from the end and adds it to new list
+            sortedData.add(data.get(i));
+        }
+        return sortedData;
     }
 }
